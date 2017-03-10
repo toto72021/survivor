0ee779cc6b3f59ea26e2df57d55ad1f6c569ef68
DESKTOP-APK398M\totoyan <toto72021@gmail.com>
2017-03-10 18:52:38 +0800

    增加排名榜

---
+++ getScore.php
@@ -0,0 +1,31 @@
+﻿<?php
+$servername = "bdm26409811.my3w.com";
+$username = "bdm26409811";	
+$password = "q402788830";
+$database = "bdm26409811_db";
+
+//连接数据库
+$conn = mysql_connect($servername,$username,$password);
+if (!$conn) {
+    echo("Connection failed: " . mysqli_connect_error());
+}
+
+//选择数据库
+mysql_select_db("$database", $conn);
+
+//输出排行榜
+$sql = "SELECT * FROM lb ORDER BY score DESC";
+$result = mysql_query($sql);
+$count = 0;
+$myRank = 0;
+while($rows = mysql_fetch_array($result)){
+	$count ++;
+	if($_POST['name'] == $rows['name'])
+		$myRank = $count;
+	
+	echo  $rows['name'] . "|" . $rows['score'] . "|" . $count ."," ;
+}
+echo "myRank:".$myRank;
+
+mysql_close($conn);
+?>
---
+++ saveScore.php
@@ -0,0 +1,24 @@
+﻿<?php
+$servername = "bdm26409811.my3w.com";
+$username = "bdm26409811";	
+$password = "q402788830";
+$database = "bdm26409811_db";
+
+//连接数据库
+$conn = mysql_connect($servername,$username,$password);
+if (!$conn) {
+    echo("Connection failed: " . mysqli_connect_error());
+}
+//选择数据库
+mysql_select_db("$database", $conn);
+
+//插入值
+$sql="INSERT INTO lb (name,score)VALUES('$_POST[name]','$_POST[score]')";
+if (!mysql_query($sql,$conn)){
+	die('Error: ' . mysql_error());
+  }else {
+	echo "insert";	  
+  }
+
+ mysql_close($conn); 
+?>
